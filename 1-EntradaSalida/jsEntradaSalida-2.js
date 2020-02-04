e82918b47d3c983c2cf1eafee538bf0958ed82cf
alumno <alumno@utnfra.com.ar>
2020-02-04 12:27:21 -0300

    clase 1

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,13 @@
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	
+    var nombre;
+    var edad;
+
+  nombre=prompt("ingrese su nombre");
+
+  edad=prompt("ingrese su edad")
+
+    alert("usted se llama"+nombre+"y tiene"+edad+"años");
 }
 
--- 1-EntradaSalida/jsEntradaSalida-4.js
+++ 1-EntradaSalida/jsEntradaSalida-4.js
@@ -2,8 +2,13 @@
 	Debemos lograr tomar un dato por 'PROMPT' 
 	y lo muestro por 'getElementById' al presionar el botón 'mostrar'
 */
+
 function mostrar()
 {
+var nombre;
 
+nombre = prompt("escribe tu nombre");
+
+document.getelementbyid("elNombre").value = nombre
 }
 
